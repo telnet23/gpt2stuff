32cf7ccb7a2c5ff480c5bd4876b003fa20abfaf0
underwood <54200401+thunderpoot@users.noreply.github.com>
2020-09-27 01:59:22 +0100

    Update train.py

--- train.py
+++ train.py
@@ -16,16 +16,14 @@ if not os.path.isdir(os.path.join("models", model_name)):
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
 
-file_name = "relay.log.stitched"
+file_name = "input.txt"   # your input file
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction=0.77
 config.graph_options.rewrite_options.layout_optimizer = rewriter_config_pb2.RewriterConfig.OFF
-# Memory allocation stuff
 
 sess = tf.compat.v1.Session(config=config)
-# sess = gpt2.start_tf_sess()
 
 gpt2.finetune(sess,
               file_name,
