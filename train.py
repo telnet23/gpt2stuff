7028ebffbac895a47460916c1ae9a2aa039c5878
underwood <54200401+thunderpoot@users.noreply.github.com>
2020-09-27 01:45:52 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,35 @@
+import gpt_2_simple as gpt2
+import os
+import requests
+import tensorflow as tf
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.python.client import device_lib
+
+model_name = "124M"
+# model_name = "779M"
+# model_name = "1558M"
+
+# Don't use the huge one without multiple GPUs and stupid amounts of memory
+
+if not os.path.isdir(os.path.join("models", model_name)):
+	print(f"Downloading {model_name} model...")
+	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
+
+
+file_name = "relay.log.stitched"
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction=0.77
+config.graph_options.rewrite_options.layout_optimizer = rewriter_config_pb2.RewriterConfig.OFF
+# Memory allocation stuff
+
+sess = tf.compat.v1.Session(config=config)
+# sess = gpt2.start_tf_sess()
+
+gpt2.finetune(sess,
+              file_name,
+              model_name=model_name,
+              steps=1000)   # steps is max number of training steps
+
+gpt2.generate(sess)
